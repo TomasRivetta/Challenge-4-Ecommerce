80636d878153705a4b03cf95bb91a11e9f822f7f
TomasRivetta <totorivetta@gmail.com>
2022-12-10 16:36:49 -0300

    creando login y registro

---
+++ js/registro.js
@@ -0,0 +1,15 @@
+import { clientServices } from "./client-service.js";
+
+const formulario = document.querySelector("[data-form]")
+
+formulario.addEventListener("submit",(evento) => {
+    evento.preventDefault();
+    const nombre = document.querySelector("[data-nombre]").value;
+    const email = document.querySelector("[data-email]").value;
+
+    console.log(nombre," - ",email);
+
+    clientServices.crearCliente(nombre,email).then(() => {
+        window.location.href = "/html/productos.html"
+    }).catch(err => console.log(err));
+});
